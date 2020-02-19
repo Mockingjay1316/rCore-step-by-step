9f2862bc321fcc88d84b8cf48e82245c4800c6e0
Cao Dingyuan <965603699@qq.com>
2020-02-19 16:47:25 +0800

    Strip C runtime dependency.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
 #![no_std]
+#![no_main]
 use core::panic::PanicInfo;
-
-fn main() {
-    // println!("Hello, world!");
-    // println! require OS support.
-}
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop{}
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
