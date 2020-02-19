2c4f05957682b37d9f1ba3dcb66bb6210ff15084
Cao Dingyuan <965603699@qq.com>
2020-02-19 22:15:49 +0800

    Add handler for timer interrupt.

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,9 +1,18 @@
 use riscv::register::{
-    scause,
+    scause::{
+        self,
+        Trap,
+        Exception,
+        Interrupt
+    },
     sepc,
     stvec,
     sscratch,
     sstatus
+};
+use crate::timer::{
+    TICKS,
+    clock_set_next_event
 };
 use crate::context::TrapFrame;
 
@@ -32,6 +41,38 @@ fn rust_trap(tf: &mut TrapFrame) {
     // let cause = tf.scause.bits();
     // let epc = tf.sepc;
     // println!("trap: cause: {:?}, epc: 0x{:#x}", cause, epc);
-    println!("rust_trap!");
-    tf.sepc += 2;
+    // println!("rust_trap!");
+    match tf.scause.cause() {
+        // 断点中断
+        Trap::Exception(Exception::Breakpoint) => breakpoint(&mut tf.sepc),
+        // S态时钟中断
+        Trap::Interrupt(Interrupt::SupervisorTimer) => super_timer(),
+        _ => panic!("undefined trap!")
+    }
+}
+
+fn breakpoint(sepc: &mut usize) {
+    println!("a breakpoint set @0x{:x}", sepc);
+    *sepc += 2;
+}
+
+fn super_timer() {
+    // 设置下一次时钟中断触发时间
+    clock_set_next_event();
+    unsafe {
+        // 更新时钟中断触发计数
+        // 注意由于 TICKS 是 static mut 的
+        // 后面会提到，多个线程都能访问这个变量
+        // 如果同时进行 +1 操作，会造成计数错误或更多严重bug
+        // 因此这是 unsafe 的，不过目前先不用管这个
+        TICKS += 1;
+        // 每触发 100 次时钟中断将计数清零并输出
+        if (TICKS == 100) {
+            TICKS = 0;
+            println!("* 100 ticks *");
+        }
+    }
+    // 由于一般都是在死循环内触发时钟中断
+    // 因此我们同样的指令再执行一次也无妨
+    // 因此不必修改 sepc
 }
