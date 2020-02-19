232061dab84cd47e12efdf048b811641cd6c3bd8
Cao Dingyuan <965603699@qq.com>
2020-02-19 18:47:09 +0800

    Breakdown main.rs into files.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,5 @@
 #![no_std]
 #![no_main]
-#![feature(asm)]
-#![feature(global_asm)]
 
-use core::panic::PanicInfo;
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
-}
-
-global_asm!(include_str!("boot/entry64.asm"));
-
-pub fn console_putchar(ch: u8) {
-    let ret: usize;
-    let arg0: usize = ch as usize;
-    let arg1: usize = 0;
-    let arg2: usize = 0;
-    let which: usize = 1;
-    unsafe {
-        asm!("ecall"
-             : "={x10}" (ret)
-             : "{x10}" (arg0), "{x11}" (arg1), "{x12}" (arg2), "{x17}" (which)
-             : "memory"
-             : "volatile"
-        );
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn rust_main() -> ! {
-    console_putchar(b'O');
-    console_putchar(b'K');
-    console_putchar(b'\n');
-    loop {}
-}
+#[allow(unused_imports)]
+use os;
