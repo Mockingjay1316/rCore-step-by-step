15f8bf878de60a23a4a811a28612ba701600ba75
Cao Dingyuan <965603699@qq.com>
2020-02-22 00:03:30 +0800

    Test paging.

--- src/init.rs
+++ src/init.rs
@@ -14,8 +14,8 @@ pub extern "C" fn rust_main() -> ! {
         fn bootstacktop();
         fn end();
     }
-    println!("_start vaddr = 0x{:x}", _start as usize);
-    println!("bootstacktop vaddr = 0x{:x}", bootstacktop as usize);
+    //println!("_start vaddr = 0x{:x}", _start as usize);
+    //println!("bootstacktop vaddr = 0x{:x}", bootstacktop as usize);
     println!(
         "free physical memory paddr = [{:#x}, {:#x})",
         end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR,
@@ -31,12 +31,10 @@ pub extern "C" fn rust_main() -> ! {
         ((end as usize - KERNEL_BEGIN_VADDR + KERNEL_BEGIN_PADDR) >> 12) + 1,
         PHYSICAL_MEMORY_END >> 12
     );
-    frame_allocating_test();
-    dynamic_allocating_test();
+    //frame_allocating_test();
+    //dynamic_allocating_test();
     crate::timer::init();
-    unsafe {
-        asm!("ebreak"::::"volatile");
-    }
+    //write_readonly_test();
     panic!("end of rust_main");
 }
 
@@ -82,4 +80,30 @@ fn dynamic_allocating_test() {
     let vec_addr = vec.as_ptr() as usize;
     assert!(vec_addr >= lbss && vec_addr < rbss);
     println!("vec is in section .bss!");
+}
+
+// 只读权限，却要写入
+fn write_readonly_test() {
+    extern "C" {
+        fn srodata();
+    }
+    unsafe {
+        let ptr = srodata as usize as *mut u8;
+        *ptr = 0xab;
+    }
+}
+
+// 不允许执行，非要执行
+fn execute_unexecutable_test() {
+    extern "C" {
+        fn sbss();
+    }
+    unsafe {
+        asm!("jr $0" :: "r"(sbss as usize) :: "volatile");
+    }
+}
+
+// 找不到页表项
+fn read_invalid_test() {
+    println!("{}", unsafe { *(0x12345678 as usize as *const u8) });
 }
