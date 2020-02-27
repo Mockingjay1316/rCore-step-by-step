ea327803c94cda153deec53016541d3bc79d6efa
Cao Dingyuan <965603699@qq.com>
2020-02-27 23:24:24 +0800

    Update

--- os/src/memory/mod.rs
+++ os/src/memory/mod.rs
@@ -60,6 +60,7 @@ pub fn kernel_remap() {
         bootstacktop as usize,
         MemoryAttr::new(),
         Linear::new(PHYSICAL_MEMORY_OFFSET),
+        None,
     );
     unsafe {
         memory_set.activate();
