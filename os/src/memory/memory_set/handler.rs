01ccf7825e89aa0e2f5dfa4ee3b857297a93d4ed
Cao Dingyuan <965603699@qq.com>
2020-02-27 23:25:45 +0800

    Add method for page copy.

--- os/src/memory/memory_set/handler.rs
+++ os/src/memory/memory_set/handler.rs
@@ -3,6 +3,8 @@ use super::attr::MemoryAttr;
 use crate::memory::alloc_frame; 
 use core::fmt::Debug;
 use alloc::boxed::Box;
+use crate::memory::access_pa_via_va;
+use crate::consts::PAGE_SIZE;
 
 // 定义 MemoryHandler trait
 pub trait MemoryHandler: Debug + 'static {
@@ -11,6 +13,7 @@ pub trait MemoryHandler: Debug + 'static {
     // 注意 map 并没有 pa 作为参数，因此接口实现者要给出该虚拟页要映射到哪个物理页
     fn map(&self, pt: &mut PageTableImpl, va: usize, attr: &MemoryAttr);
     fn unmap(&self, pt: &mut PageTableImpl, va: usize);
+    fn page_copy(&self, pt: &mut PageTableImpl, va: usize, src: usize, length: usize);
 }
 impl Clone for Box<dyn MemoryHandler> {
     fn clone(&self) -> Box<dyn MemoryHandler> { self.box_clone() }
@@ -32,6 +35,29 @@ impl MemoryHandler for Linear {
         attr.apply(pt.map(va, va - self.offset));
     }
     fn unmap(&self, pt: &mut PageTableImpl, va: usize) { pt.unmap(va); }
+    fn page_copy(&self, pt: &mut PageTableImpl, va: usize, src: usize, length: usize) {
+        let pa = pt.get_entry(va)
+            .expect("get pa error!")
+            .0
+            .addr()
+            .as_usize();
+        assert!(va == access_pa_via_va(pa));
+        assert!(va == pa + self.offset);
+        unsafe {
+            let dst = core::slice::from_raw_parts_mut(
+                va as *mut u8,
+                PAGE_SIZE,
+            );
+            if length > 0 {
+                let src = core::slice::from_raw_parts(
+                    src as *const u8,
+                    PAGE_SIZE,
+                );
+                for i in 0..length { dst[i] = src[i]; }
+            }
+            for i in length..PAGE_SIZE { dst[i] = 0; }
+        }
+    }
 }
 // ByFrame: 不知道映射到哪个物理页帧
 // 那我们就分配一个新的物理页帧，可以保证不会产生冲突
@@ -52,5 +78,26 @@ impl MemoryHandler for ByFrame {
     }
     fn unmap(&self, pt: &mut PageTableImpl, va: usize) {
         pt.unmap(va);
+    }
+    fn page_copy(&self, pt: &mut PageTableImpl, va: usize, src: usize, length: usize) {
+        let pa = pt.get_entry(va)
+            .expect("get pa error!")
+            .0
+            .addr()
+            .as_usize();
+        unsafe {
+            let dst = core::slice::from_raw_parts_mut(
+                access_pa_via_va(pa) as *mut u8,
+                PAGE_SIZE,
+            );
+            if length > 0 {
+                let src = core::slice::from_raw_parts(
+                    src as *const u8,
+                    PAGE_SIZE,
+                );
+                for i in 0..length { dst[i] = src[i]; }
+            }
+            for i in length..PAGE_SIZE { dst[i] = 0; }
+        }
     }
 }
