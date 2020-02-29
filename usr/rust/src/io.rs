111e037c3122670b1c3e3cffc11a797401029269
Cao Dingyuan <965603699@qq.com>
2020-02-29 18:51:06 +0800

    User function getc.

--- usr/rust/src/io.rs
+++ usr/rust/src/io.rs
@@ -1,7 +1,20 @@
 use crate::syscall::sys_write;
+use crate::syscall::sys_read;
 use core::fmt::{ self, Write };
 
+// 每个进程默认打开三个文件
+// 标准输入 stdin fd = 0
+// 标准输出 stdout fd = 1
+// 标准错误输出 stderr fd = 2
+pub const STDIN: usize = 0;
 struct Stdout;
+
+// 调用 sys_read 从标准输入读入一个字符
+pub fn getc() -> u8 {
+    let mut c = 0u8;
+    assert_eq!(sys_read(STDIN, &mut c, 1), 1);
+    c
+}
 
 // 输出一个字符
 pub fn putchar(ch: char) {
